acb49585382687030be064ee089e35b31787724f
Puru Vijay <devpuruvj@gmail.com>
2021-06-12 20:50:49 +0530

    Delete build

--- build/_app/pages/__layout.svelte-5dd3061b.js
+++
@@ -1,1 +0,0 @@
-import{S as s,i as e,s as r}from"../chunks/vendor-f3e6f2cc.js";export default class extends s{constructor(s){super(),e(this,s,null,null,r,{})}}
--- svelte.config.js
+++ svelte.config.js
@@ -1,13 +1,14 @@
 // @ts-check
-import preprocess from "svelte-preprocess";
-import staticAdapter from "@sveltejs/adapter-static";
+import preprocess from 'svelte-preprocess';
+import staticAdapter from '@sveltejs/adapter-static';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
   preprocess: preprocess(),
   kit: {
-    target: "#svelte",
+    target: '#svelte',
     adapter: staticAdapter(),
+    router: false,
   },
 };
 
